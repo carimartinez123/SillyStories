81e89a0e426b1821277019462a113859fcd7b86e
Cari Martinez <cari_martinez123@yahoo.com>
2016-03-19 14:49:03 -0600

    Updated NewStory Style

--- app/src/main/java/carimartinez123/sillystories/NewStoryActivity.java
+++ app/src/main/java/carimartinez123/sillystories/NewStoryActivity.java
@@ -14,17 +14,7 @@ public class NewStoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_story);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-    /*    Spinner newStoryDropdown = (Spinner) findViewById(R.id.newStorySpinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.new_story_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        newStoryDropdown.setAdapter(adapter);
-
-        newStoryDropdown.setOnItemSelectedListener(new SpinnerActivity());
-
-*/
 
 
 
@@ -33,7 +23,7 @@ public class NewStoryActivity extends AppCompatActivity {
     public void goToSelections(View view)
     {
         Intent intent;
-        if(view.getId() == R.id.rideButton) {
+        if(view.getId() == R.id.story1Button) {
             BookContent.setTitle("Ride to the Airport");
             intent = new Intent(this, CharacterSelectionActivity.class);
             startActivity(intent);
@@ -43,23 +33,6 @@ public class NewStoryActivity extends AppCompatActivity {
 
     }
 
-    //TODO: figure out spinners
-   /* private class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
-        @Override
-        public void onItemSelected(AdapterView<?> parent, View view,
-                                   int pos, long id) {
 
-
-            BookContent.generateText((String)parent.getItemAtPosition(pos));
-            Intent intent = new Intent(this, CharacterSelectionActivity.class);
-            startActivity(intent);
-
-
-        }
-        @Override
-        public void onNothingSelected(AdapterView<?> parent) {
-
-        }
-    }*/
 
 }
