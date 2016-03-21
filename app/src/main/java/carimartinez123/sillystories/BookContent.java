d06dba458e9dbc0ce86d5b2bbdbf129a7d35e1d4
Cari Martinez <cari_martinez123@yahoo.com>
2016-03-20 19:49:35 -0600

    added character images; updated characterSelection

--- app/src/main/java/carimartinez123/sillystories/BookContent.java
+++ app/src/main/java/carimartinez123/sillystories/BookContent.java
@@ -23,7 +23,7 @@ public class BookContent {
     public static void generateGrammarTypes(String str)
     {
         numChars = 4;
-        numNouns = 8;
+        numNouns = 9;
         numAdjs = 8;
         numNums = 2;
         numPlaces = 1;
@@ -40,8 +40,9 @@ public class BookContent {
                 String CH2 = chars.get(1);
                 String CH3 = chars.get(2);
                 String CH4 = chars.get(3);
-                String VEH1 = "___";
+                
                 String PL = "___";
+                
                 String ADJ1 = adjs.get(0);
                 String ADJ2 = adjs.get(0);
                 String ADJ3 = adjs.get(0);
@@ -50,6 +51,8 @@ public class BookContent {
                 String ADJ6 = "___";
                 String ADJ7 = "___";
                 String ADJ8 = "___";
+                
+                String N0 = "___";
                 String N1 = nouns.get(0);
                 String N2 = "___";
                 String N3 = "___";
@@ -58,14 +61,17 @@ public class BookContent {
                 String N6 = "___";
                 String N7 = "___";
                 String N8 = "___";
+                
                 String NUM1 = "___";
                 String NUM2 = "___";
+                
                 String ADV1 = "___";
                 String ADV2 = "___";
+                
                 String WEATHER1 = "___";
 
 
-                text = "One day, " + CH1 + " and " + CH2 + " got in their " + VEH1 + " to get to the airport. They were going on a trip to " + PL + ", and they were very " + ADJ1 + ".  They were riding along " + ADV1 + " when all of a sudden " + CH1 + " said, \"Oh no I forgot my " + ADJ2 + " " + N1 + "!\" So, they turned their " + VEH1 + " around and started back to the house. " + CH2 + " said \"Uh-oh. We seem to be slowing down.\" \"Oh no!\" said " + CH1 + ". \" I forgot to put gas in the " + VEH1 + "!\" So, they stopped at the " + ADJ3 +  " gas station, " + CH2 + " said, \"Wow! This " + ADJ3 + " gas sure costs a lot of " +  N2 + "!\" " + CH1 +  " yelled, \"Oh no! I forgot my " + N2 + "!\" Now, they were really " + ADJ4 + ". Just then, it started to rain. " + CH2 + " cried, \"Look out! A " + WEATHER1 + "!\"  " + CH2 + " said, \" Ugh! I hate " + WEATHER1 + "s!\" " + CH1 +   "said, \"Don't worry. We'll get out of here. I found my lucky " + N3 + ". We can pay with that.\" "+ CH1 + " took the lucky " + N3 + " to " + CH3 + ", the cashier, who looked at it " + ADV2 + ". \"What exactly is this?\", said " + CH3 + ". \" This is my lucky" +  N3 + ",\" said " + CH1 + ", and it's worth " + NUM1 + " dollars, so I'm sure it will pay for the gas we need for our " + VEH1 + ".\" \"Okie dokie\", said " + CH3 + ". \"Here is your change.\"" +  CH3 + " gave " + CH1 + " one " + N4 + ", and " + CH1 + " and " + CH2 + " were off. \"Oh no!\" said " + CH1 + ". \"Look at the time! We're going to be late for our airplane if we don't go get my N1 right now!\" So they got in their " + VEH1 + " and went as fast as they could. All of a sudden, " + CH2 + "saw some flashing lights.\"Uh-oh!\" said " + CH2 + ". \"I think we were going too fast! The police officer is going to give us a " + N4 + "!\"" +  CH4 + ", the police officer, said \"Do you know how fast you were going? " + NUM2 + " miles per hour! I am going to have to give you a " + ADJ5 + " " +  N4 + ".\"" +  CH1 + " said, \"I'm sorry. We are in a hurry because I forgot my " + N1 + ", and we have to get to the airport fast. We're late! We're going to " + PL + " and we don't want to miss our flight. \" Well\", said " + CH4 + ", I happen to have an extra " + ADJ6 + " " +  N1 + " right here. I guess you could borrow it for a while if you promise to take good care of it.\" \"WE PROMISE!\" said" + CH1 + " and " + CH2+ ". Then, " + CH4 + "said \"You know, I know a " + ADJ7 + " shortcut to the airport from here. Follow me!\" So, " + CH1 + " and " + CH2 + " followed " + CH4 + " over the " + N5 + ", under the " + N6 + ", across the " + N7 + "and through the" + ADJ8 + " " + N8 + ". Finally, they made it to the airport. Time to fly!";
+                text = "One day, " + CH1 + " and " + CH2 + " got in their " + N0 + " to get to the airport. They were going on a trip to " + PL + ", and they were very " + ADJ1 + ".  They were riding along " + ADV1 + " when all of a sudden " + CH1 + " said, \"Oh no I forgot my " + ADJ2 + " " + N1 + "!\" So, they turned their " + N0 + " around and started back to the house. " + CH2 + " said \"Uh-oh. We seem to be slowing down.\" \"Oh no!\" said " + CH1 + ". \" I forgot to put gas in the " + N0 + "!\" So, they stopped at the " + ADJ3 +  " gas station, " + CH2 + " said, \"Wow! This " + ADJ3 + " gas sure costs a lot of " +  N2 + "!\" " + CH1 +  " yelled, \"Oh no! I forgot my " + N2 + "!\" Now, they were really " + ADJ4 + ". Just then, it started to rain. " + CH2 + " cried, \"Look out! A " + WEATHER1 + "!\"  " + CH2 + " said, \" Ugh! I hate " + WEATHER1 + "s!\" " + CH1 +   "said, \"Don't worry. We'll get out of here. I found my lucky " + N3 + ". We can pay with that.\" "+ CH1 + " took the lucky " + N3 + " to " + CH3 + ", the cashier, who looked at it " + ADV2 + ". \"What exactly is this?\", said " + CH3 + ". \" This is my lucky" +  N3 + ",\" said " + CH1 + ", and it's worth " + NUM1 + " dollars, so I'm sure it will pay for the gas we need for our " + N0 + ".\" \"Okie dokie\", said " + CH3 + ". \"Here is your change.\"" +  CH3 + " gave " + CH1 + " one " + N4 + ", and " + CH1 + " and " + CH2 + " were off. \"Oh no!\" said " + CH1 + ". \"Look at the time! We're going to be late for our airplane if we don't go get my N1 right now!\" So they got in their " + N0 + " and went as fast as they could. All of a sudden, " + CH2 + "saw some flashing lights.\"Uh-oh!\" said " + CH2 + ". \"I think we were going too fast! The police officer is going to give us a " + N4 + "!\"" +  CH4 + ", the police officer, said \"Do you know how fast you were going? " + NUM2 + " miles per hour! I am going to have to give you a " + ADJ5 + " " +  N4 + ".\"" +  CH1 + " said, \"I'm sorry. We are in a hurry because I forgot my " + N1 + ", and we have to get to the airport fast. We're late! We're going to " + PL + " and we don't want to miss our flight. \" Well\", said " + CH4 + ", I happen to have an extra " + ADJ6 + " " +  N1 + " right here. I guess you could borrow it for a while if you promise to take good care of it.\" \"WE PROMISE!\" said" + CH1 + " and " + CH2+ ". Then, " + CH4 + "said \"You know, I know a " + ADJ7 + " shortcut to the airport from here. Follow me!\" So, " + CH1 + " and " + CH2 + " followed " + CH4 + " over the " + N5 + ", under the " + N6 + ", across the " + N7 + "and through the" + ADJ8 + " " + N8 + ". Finally, they made it to the airport. Time to fly!";
 
                 System.out.println(text);
                 break;
--- app/src/main/java/carimartinez123/sillystories/CharacterSelectionActivity.java
+++ app/src/main/java/carimartinez123/sillystories/CharacterSelectionActivity.java
@@ -1,21 +1,33 @@
 package carimartinez123.sillystories;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class CharacterSelectionActivity extends AppCompatActivity {
 
-    private boolean beastBoySelected = false;
-    private boolean starfireSelected = false;
-    private boolean ravenSelected = false;
-    private boolean cyborgSelected = false;
-    private boolean robinSelected = false;
+    private boolean char0Selected = false;
+    private boolean char1Selected = false;
+    private boolean char2Selected = false;
+    private boolean char3Selected = false;
+    private boolean char4Selected = false;
+    private boolean char5Selected = false;
+    private boolean char6Selected = false;
+    private boolean char7Selected = false;
+    private boolean char8Selected = false;
+    
+    private String char0 = "Starfire";
+    private String char1 = "Beast Boy";
+    private String char2 = "Raven";
+    private String char3 = "Cyborg";
+    private String char4 = "Robin";
+    private String char5 = "Squid Boy";
+    private String char6 = "Splat Girl";
+    private String char7 = "Link";
+    private String char8 = "Zelda";
     
     private TextView charNum;
     @Override
@@ -65,42 +77,70 @@ public class CharacterSelectionActivity extends AppCompatActivity {
     public void getCharacters(View view)
     {
 
-        if(view.getId() == R.id.beastboyButton && !beastBoySelected) {
-            beastBoySelected = true;
-            BookContent.getChars().add("Beast Boy");
+        if(view.getId() == R.id.charButton0 && !char0Selected) {
+            char0Selected = true;
+            BookContent.getChars().add(char0);
             updateCharNum();
 
         }
-        else if(view.getId() == R.id.starfireButton && !starfireSelected) {
-            starfireSelected = true;
-            BookContent.getChars().add("Starfire");
-            updateCharNum();
-
-
-        }
-        else if(view.getId() == R.id.ravenButton && !ravenSelected) {
-            ravenSelected = true;
-            BookContent.getChars().add("Raven");
-            updateCharNum();
-
-        }
-        else if(view.getId() == R.id.cyborgButton && !cyborgSelected) {
-            cyborgSelected = true;
-            BookContent.getChars().add("Cyborg");
-            updateCharNum();
-
-        }
-        else if(view.getId() == R.id.robinButton && !robinSelected) {
-            robinSelected = true;
-            BookContent.getChars().add("Robin");
+        else if(view.getId() == R.id.charButton1 && !char1Selected) {
+            char1Selected = true;
+            BookContent.getChars().add(char1);
             updateCharNum();
 
 
         }
+        else if(view.getId() == R.id.charButton2 && !char2Selected) {
+            char2Selected = true;
+            BookContent.getChars().add(char2);
+            updateCharNum();
 
+        }
+        else if(view.getId() == R.id.charButton3 && !char3Selected) {
+            char3Selected = true;
+            BookContent.getChars().add(char3);
+            updateCharNum();
+
+        }
+        else if(view.getId() == R.id.charButton4 && !char6Selected) {
+            char6Selected = true;
+            BookContent.getChars().add(char4);
+            updateCharNum();
+
+
+        }
+        else if(view.getId() == R.id.charButton5 && !char5Selected) {
+            char5Selected = true;
+            BookContent.getChars().add(char5);
+            updateCharNum();
+            
+        }
+        else if(view.getId() == R.id.charButton6 && !char6Selected) {
+            char6Selected = true;
+            BookContent.getChars().add(char6);
+            updateCharNum();
+
+
+        }
+
+        else if(view.getId() == R.id.charButton7 && !char7Selected) {
+            char7Selected = true;
+            BookContent.getChars().add(char7);
+            updateCharNum();
+
+
+        }
+
+        else if(view.getId() == R.id.charButton8 && !char8Selected) {
+            char8Selected = true;
+            BookContent.getChars().add(char8);
+            updateCharNum();
+
+
+        }
         else if(view.getId() == R.id.doneCharsButton)
         {
-            int i = 0;
+
             if(BookContent.getChars().size() != BookContent.getNumChars()) {
 
             }
