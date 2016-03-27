44d7800480629aa460f79511ee75b1bed79d1c22
Cari Martinez <cari_martinez123@yahoo.com>
2016-03-26 21:14:27 -0600

    update character selection to show selections

--- app/src/main/java/carimartinez123/sillystories/CharacterSelectionActivity.java
+++ app/src/main/java/carimartinez123/sillystories/CharacterSelectionActivity.java
@@ -1,11 +1,13 @@
 package carimartinez123.sillystories;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class CharacterSelectionActivity extends AppCompatActivity {
@@ -29,6 +31,16 @@ public class CharacterSelectionActivity extends AppCompatActivity {
     private String char6;
     private String char7;
     private String char8;
+
+    private FrameLayout charFrame0;
+    private FrameLayout charFrame1;
+    private FrameLayout charFrame2;
+    private FrameLayout charFrame3;
+    private FrameLayout charFrame4;
+    private FrameLayout charFrame5;
+    private FrameLayout charFrame6;
+    private FrameLayout charFrame7;
+    private FrameLayout charFrame8;
     
     private TextView charNum;
     @Override
@@ -39,6 +51,15 @@ public class CharacterSelectionActivity extends AppCompatActivity {
         charNum.setText("Pick " + (BookContent.getNumChars() - BookContent.getChars().size()) + " characters:");
         Button doneButton = (Button) findViewById(R.id.doneCharsButton);
         doneButton.setEnabled(false);
+        charFrame0 = (FrameLayout)findViewById(R.id.charFrame0);
+        charFrame1 = (FrameLayout)findViewById(R.id.charFrame1);
+        charFrame2 = (FrameLayout)findViewById(R.id.charFrame2);
+        charFrame3 = (FrameLayout)findViewById(R.id.charFrame3);
+        charFrame4 = (FrameLayout)findViewById(R.id.charFrame4);
+        charFrame5 = (FrameLayout)findViewById(R.id.charFrame5);
+        charFrame6 = (FrameLayout)findViewById(R.id.charFrame6);
+        charFrame7 = (FrameLayout)findViewById(R.id.charFrame7);
+        charFrame8 = (FrameLayout)findViewById(R.id.charFrame8);
         char0 = getString(R.string.char0);
         char1 = getString(R.string.char1);
         char2 = getString(R.string.char2);
@@ -86,67 +107,180 @@ public class CharacterSelectionActivity extends AppCompatActivity {
     {
 
         Log.d("TEST", "numChars = " + BookContent.getNumChars());
-        if(view.getId() == R.id.charButton0 && !char0Selected) {
-            char0Selected = true;
-            BookContent.getChars().add(char0);
+        if(view.getId() == R.id.charButton0) {
+            if(!char0Selected && (BookContent.getChars().size() < BookContent.getNumChars()))
+            {
+                char0Selected = true;
+                charFrame0.setBackgroundColor(Color.YELLOW);
+                BookContent.getChars().add(char0);
+            }
+            else
+            {
+                if(char0Selected) 
+                {
+                    char0Selected = false;
+                    charFrame0.setBackgroundColor(Color.WHITE);
+                    BookContent.getChars().remove(char0);
+                }
+            }
             updateCharNum();
 
         }
-        else if(view.getId() == R.id.charButton1 && !char1Selected) {
-            char1Selected = true;
-            BookContent.getChars().add(char1);
-            updateCharNum();
 
+        else if(view.getId() == R.id.charButton1) {
+            if(!char1Selected && (BookContent.getChars().size() < BookContent.getNumChars()))
+            {
+                char1Selected = true;
+                charFrame1.setBackgroundColor(Color.YELLOW);
+                BookContent.getChars().add(char1);
+            }
+            else
+            {
+                if(char1Selected)
+                {
+                    char1Selected = false;
+                    charFrame1.setBackgroundColor(Color.WHITE);
+                    BookContent.getChars().remove(char1);
+
+                }
+            }
+            updateCharNum();
 
         }
-        else if(view.getId() == R.id.charButton2 && !char2Selected) {
-            char2Selected = true;
-            BookContent.getChars().add(char2);
+        else if(view.getId() == R.id.charButton2) {
+            if(!char2Selected && (BookContent.getChars().size() < BookContent.getNumChars()))
+            {
+                char2Selected = true;
+                charFrame2.setBackgroundColor(Color.YELLOW);
+                BookContent.getChars().add(char2);
+            }
+            else
+            {
+                if(char2Selected) {
+                    char2Selected = false;
+                    charFrame2.setBackgroundColor(Color.WHITE);
+                    BookContent.getChars().remove(char2);
+                }
+            }
             updateCharNum();
-
-        }
-        else if(view.getId() == R.id.charButton3 && !char3Selected) {
-            char3Selected = true;
-            BookContent.getChars().add(char3);
-            updateCharNum();
-
-        }
-        else if(view.getId() == R.id.charButton4 && !char4Selected) {
-            char4Selected = true;
-            BookContent.getChars().add(char4);
-            updateCharNum();
-
-
-        }
-        else if(view.getId() == R.id.charButton5 && !char5Selected) {
-            char5Selected = true;
-            BookContent.getChars().add(char5);
-            updateCharNum();
-            
-        }
-        else if(view.getId() == R.id.charButton6 && !char6Selected) {
-            char6Selected = true;
-            BookContent.getChars().add(char6);
-            updateCharNum();
-
-
-        }
-
-        else if(view.getId() == R.id.charButton7 && !char7Selected) {
-            char7Selected = true;
-            BookContent.getChars().add(char7);
-            updateCharNum();
-
 
         }
 
-        else if(view.getId() == R.id.charButton8 && !char8Selected) {
-            char8Selected = true;
-            BookContent.getChars().add(char8);
+        else if(view.getId() == R.id.charButton3) {
+            if(!char3Selected && (BookContent.getChars().size() < BookContent.getNumChars()))
+            {
+                char3Selected = true;
+                charFrame3.setBackgroundColor(Color.YELLOW);
+                BookContent.getChars().add(char3);
+            }
+            else
+            {
+                if(char3Selected) {
+                    char3Selected = false;
+                    charFrame3.setBackgroundColor(Color.WHITE);
+                    BookContent.getChars().remove(char3);
+                }
+            }
             updateCharNum();
 
+        }
+
+        else if(view.getId() == R.id.charButton4) {
+            if(!char4Selected && (BookContent.getChars().size() < BookContent.getNumChars()))
+            {
+                char4Selected = true;
+                charFrame4.setBackgroundColor(Color.YELLOW);
+                BookContent.getChars().add(char4);
+            }
+            else
+            {
+                if(char4Selected) {
+                    char4Selected = false;
+                    charFrame4.setBackgroundColor(Color.WHITE);
+                    BookContent.getChars().remove(char4);
+                }
+            }
+            updateCharNum();
 
         }
+
+        else if(view.getId() == R.id.charButton5) {
+            if(!char5Selected && (BookContent.getChars().size() < BookContent.getNumChars()))
+            {
+                char5Selected = true;
+                charFrame5.setBackgroundColor(Color.YELLOW);
+                BookContent.getChars().add(char5);
+            }
+            else
+            {
+                if(char5Selected) {
+                    char5Selected = false;
+                    charFrame5.setBackgroundColor(Color.WHITE);
+                    BookContent.getChars().remove(char5);
+                }
+            }
+            updateCharNum();
+
+        }
+
+        else if(view.getId() == R.id.charButton6) {
+            if(!char6Selected && (BookContent.getChars().size() < BookContent.getNumChars()))
+            {
+                char6Selected = true;
+                charFrame6.setBackgroundColor(Color.YELLOW);
+                BookContent.getChars().add(char6);
+            }
+            else
+            {
+                if(char6Selected) {
+                    char6Selected = false;
+                    charFrame6.setBackgroundColor(Color.WHITE);
+                    BookContent.getChars().remove(char6);
+                }
+            }
+            updateCharNum();
+
+        }
+
+        else if(view.getId() == R.id.charButton7) {
+            if(!char7Selected && (BookContent.getChars().size() < BookContent.getNumChars()))
+            {
+                char7Selected = true;
+                charFrame7.setBackgroundColor(Color.YELLOW);
+                BookContent.getChars().add(char7);
+            }
+            else
+            {
+                if(char7Selected) {
+                    char7Selected = false;
+                    charFrame7.setBackgroundColor(Color.WHITE);
+                    BookContent.getChars().remove(char7);
+                }
+            }
+            updateCharNum();
+
+        }
+
+        else if(view.getId() == R.id.charButton8) {
+            if(!char8Selected && (BookContent.getChars().size() < BookContent.getNumChars()))
+            {
+                char8Selected = true;
+                charFrame8.setBackgroundColor(Color.YELLOW);
+                BookContent.getChars().add(char8);
+            }
+            else
+            {
+                if(char8Selected) {
+                    char8Selected = false;
+                    charFrame8.setBackgroundColor(Color.WHITE);
+                    BookContent.getChars().remove(char8);
+                }
+            }
+            updateCharNum();
+
+        }
+
+
         else if(view.getId() == R.id.doneCharsButton)
         {
 
