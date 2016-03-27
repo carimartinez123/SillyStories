f1796d03c6fc6fc38a00313afd97832bb6d67c23
Cari Martinez <cari_martinez123@yahoo.com>
2016-03-27 14:53:21 -0600

    updated Adjective Selection

--- app/src/main/java/carimartinez123/sillystories/AdjectiveSelectionActivity.java
+++ app/src/main/java/carimartinez123/sillystories/AdjectiveSelectionActivity.java
@@ -1,6 +1,7 @@
 package carimartinez123.sillystories;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,34 +12,78 @@ import android.widget.TextView;
 public class AdjectiveSelectionActivity extends AppCompatActivity {
 
 
-    private boolean adj0Selected = false;
-    private boolean adj1Selected = false;
-    private boolean adj2Selected = false;
-    private boolean adj3Selected = false;
-    private boolean adj4Selected = false;
-    private boolean adj5Selected = false;
-    private boolean adj6Selected = false;
-    private boolean adj7Selected = false;
-    private boolean adj8Selected = false;
+    private final int SELECTABLE_ADJS = 12;
 
+    private boolean [] adjSelected = new boolean[SELECTABLE_ADJS];
+
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button button10;
+    private Button button11;
+    private Button doneButton;
     private TextView adjNum;
+    private Button[] buttons = new Button[SELECTABLE_ADJS];
+    private String[] adjs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adjective_selection);
+        Resources res = getResources();
+        adjs = res.getStringArray(R.array.adj_array);
+        button0 = (Button)findViewById(R.id.adj0Button);
+        button1 = (Button)findViewById(R.id.adj1Button);
+        button2 = (Button)findViewById(R.id.adj2Button);
+        button3 = (Button)findViewById(R.id.adj3Button);
+        button4 = (Button)findViewById(R.id.adj4Button);
+        button5 = (Button)findViewById(R.id.adj5Button);
+        button6 = (Button)findViewById(R.id.adj6Button);
+        button7 = (Button)findViewById(R.id.adj7Button);
+        button8 = (Button)findViewById(R.id.adj8Button);
+        button9 = (Button)findViewById(R.id.adj9Button);
+        button10 = (Button)findViewById(R.id.adj10Button);
+        button11 = (Button)findViewById(R.id.adj11Button);
+        doneButton = (Button) findViewById(R.id.doneAdjsButton);
         adjNum = (TextView) findViewById(R.id.adjNumTextView);
-        adjNum.setText("Pick " + (BookContent.getNumAdjs() - BookContent.getAdjs().size()) + " Adjectives:");
-        Button doneButton = (Button) findViewById(R.id.doneAdjsButton);
+
+
+        buttons[0] = button0;
+        buttons[1] = button1;
+        buttons[2] = button2;
+        buttons[3] = button3;
+        buttons[4] = button4;
+        buttons[5] = button5;
+        buttons[6] = button6;
+        buttons[7] = button7;
+        buttons[8] = button8;
+        buttons[9] = button9;
+        buttons[10] = button10;
+        buttons[11] = button11;
+
+        adjSelected[0] = false;
+        adjSelected[1] = false;
+        adjSelected[2] = false;
+        adjSelected[3] = false;
+        adjSelected[4] = false;
+        adjSelected[5] = false;
+        adjSelected[6] = false;
+        adjSelected[7] = false;
+        adjSelected[8] = false;
+        adjSelected[9] = false;
+        adjSelected[10] = false;
+        adjSelected[11] = false;
+
         doneButton.setEnabled(false);
-
-
-    }
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-
+        Utils.setButtonText(buttons, adjs);
+        Utils.updateWordTypeNum(adjNum, doneButton, BookContent.getNumAdjs(), BookContent.getAdjs().size());
 
     }
     @Override
@@ -47,143 +92,39 @@ public class AdjectiveSelectionActivity extends AppCompatActivity {
         //disable back button
     }
 
-    private void updateAdjNum()
-    {
-        int adjsLeft = BookContent.getNumAdjs() - BookContent.getAdjs().size();
-        if(adjsLeft > 1) {
-            adjNum.setText("Pick " + adjsLeft + " Adjectives:");
-        }
-        else if (adjsLeft == 1)
-        {
-            adjNum.setText("Pick " + adjsLeft + " Adjective:");
-        }
-        else
-        {
-            adjNum.setText("Click done!");
-            Button doneButton = (Button) findViewById(R.id.doneAdjsButton);
-            doneButton.setEnabled(true);
-        }
-    }
-
-    public void getAdjs(View view)
-    {
-
-        if(view.getId() == R.id.adj0Button && !adj0Selected) {
-            adj0Selected = true;
-            BookContent.getAdjs().add(getString(R.string.adj0));
-            updateAdjNum();
-
-        }
-        else if(view.getId() == R.id.adj1Button && !adj1Selected) {
-            adj1Selected = true;
-            BookContent.getAdjs().add(getString(R.string.adj1));
-            updateAdjNum();
-
-
-        }
-        else if(view.getId() == R.id.adj2Button && !adj2Selected) {
-            adj2Selected = true;
-            BookContent.getAdjs().add(getString(R.string.adj2));
-            updateAdjNum();
-
-        }
-        else if(view.getId() == R.id.adj3Button && !adj3Selected) {
-            adj3Selected = true;
-            BookContent.getAdjs().add(getString(R.string.adj3));
-            updateAdjNum();
-
-        }
-        else if(view.getId() == R.id.adj4Button && !adj4Selected) {
-            adj4Selected = true;
-            BookContent.getAdjs().add(getString(R.string.adj4));
-            updateAdjNum();
-
-
-        }
-        else if(view.getId() == R.id.adj5Button && !adj5Selected) {
-            adj5Selected = true;
-            BookContent.getAdjs().add(getString(R.string.adj5));
-            updateAdjNum();
-
-        }
-        else if(view.getId() == R.id.adj6Button && !adj6Selected) {
-            adj6Selected = true;
-            BookContent.getAdjs().add(getString(R.string.adj6));
-            updateAdjNum();
-
-
-        }
-
-        else if(view.getId() == R.id.adj7Button && !adj7Selected) {
-            adj7Selected = true;
-            BookContent.getAdjs().add(getString(R.string.adj7));
-            updateAdjNum();
-
-
-        }
-
-        else if(view.getId() == R.id.adj8Button && !adj8Selected) {
-            adj8Selected = true;
-            BookContent.getAdjs().add(getString(R.string.adj8));
-            updateAdjNum();
-
-
-        }
-        else if(view.getId() == R.id.doneAdjsButton)
-        {
-
-            if(BookContent.getAdjs().size() < BookContent.getNumAdjs()) {
-                Log.d("TEST", "Size of BookContent.adj less than required number of adjs for this story!");
-
-            }
-            else {
-                Intent intent = new Intent(this, BookActivity.class);
-                startActivity(intent);
-            }
-        }
-
-    }
-
-}
-/*
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_adjective_selection);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public void onResume()
+    {
+        super.onResume();
+
+
     }
+
+
     public void getAdjs(View view)
     {
 
-        if(view.getId() == R.id.stinkyButton) {
+        int index_pressed = Utils.getIndex(buttons, view);
+        boolean needWords = BookContent.getAdjs().size() < BookContent.getNumAdjs();
+        Utils.selectWord(needWords, adjSelected, buttons, index_pressed, BookContent.getAdjs(), adjs);
+        Utils.updateWordTypeNum(adjNum, doneButton, BookContent.getNumAdjs(), BookContent.getAdjs().size());
 
-            BookContent.getAdjs().add("stinky");
+
+    }
+
+    public void adjsDone(View view)
+    {
+
+        if(BookContent.getAdjs().size() < BookContent.getNumAdjs()) {
+            Log.d("TEST", "Size of BookContent.adj less than required number of adjs for this story!");
 
         }
-        else if(view.getId() == R.id.sillyButton) {
-
-            BookContent.getAdjs().add("silly");
-
-        }
-        else if(view.getId() == R.id.suspiciousButton) {
-
-            BookContent.getAdjs().add("suspicious");
-
-        }
-        else if(view.getId() == R.id.doneAdjsButton)
-        {
-            int i = 0;
-            while(i < BookContent.getAdjs().size()) {
-                System.out.println(BookContent.getAdjs().get(i));
-                i++;
-            }
+        else {
             Intent intent = new Intent(this, BookActivity.class);
             startActivity(intent);
         }
-
     }
 
 
+
 }
-*/
