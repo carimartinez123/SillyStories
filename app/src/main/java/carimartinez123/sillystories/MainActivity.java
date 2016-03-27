31b0e065b26c85e169af37c55cb71ac7c6386b46
Cari Martinez <cari_martinez123@yahoo.com>
2016-03-27 14:27:35 -0600

    updated MainActivity to show when button is pushed

--- app/src/main/java/carimartinez123/sillystories/MainActivity.java
+++ app/src/main/java/carimartinez123/sillystories/MainActivity.java
@@ -2,57 +2,55 @@ package carimartinez123.sillystories;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     public static File appDirectory;
+    private Button newButton;
+    private Button savedButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newButton = (Button)findViewById(R.id.newStoryButton);
+        savedButton = (Button)findViewById(R.id.savedStoryButton);
+        newButton.setTextColor(Color.BLACK);
+        savedButton.setTextColor(Color.BLACK);
         appDirectory = getFilesDir();
 
     }
 
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        savedButton.setTextColor(Color.BLACK);
+        newButton.setTextColor(Color.BLACK);
+
+    }
     public void startStory(View view)
     {
         Intent intent;
         if(view.getId() == R.id.savedStoryButton) {
+            savedButton.setTextColor(Color.WHITE);
             intent = new Intent(this, SavedStoryActivity.class);
         }
         else
         {
+            newButton.setTextColor(Color.WHITE);
             intent = new Intent(this, NewStoryActivity.class);
         }
         startActivity(intent);
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
