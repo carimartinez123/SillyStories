4ce37ac2d78884e05799b44c22a3e3fd16ae8ec4
Cari Martinez <cari_martinez123@yahoo.com>
2016-03-29 14:00:44 -0600

    updated unimplemented notification style

--- app/src/main/java/carimartinez123/sillystories/NewStoryActivity.java
+++ app/src/main/java/carimartinez123/sillystories/NewStoryActivity.java
@@ -3,12 +3,15 @@ package carimartinez123.sillystories;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class NewStoryActivity extends AppCompatActivity {
 
@@ -46,6 +49,20 @@ public class NewStoryActivity extends AppCompatActivity {
 
     }
 
+    public void unimplemented(View view) {
+        final Button clickedButton = (Button) view;
+        final String title = (String) clickedButton.getText();
+        new CountDownTimer(300, 300) {
 
+            public void onTick(long millisUntilFinished) {
+                clickedButton.setText("Not yet implemented!");
 
+            }
+
+            public void onFinish() {
+                clickedButton.setText(title);
+
+            }
+        }.start();
+    }
 }
